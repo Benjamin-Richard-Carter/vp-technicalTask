1de6dfbf5c99dfd3b11e916df68516b2d79db46c
BenTheMexican <bencarter@outlook.com>
2024-10-15 20:55:56 +0100

    install + setup react-router, use-query-params, query-string

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,32 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import '../index.css';
+import { QueryParamProvider } from 'use-query-params';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
+import queryString from 'query-string';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryParamProvider
+        adapter={ReactRouter6Adapter}
+        options={{
+          searchStringToObject: queryString.parse,
+          objectToSearchString: queryString.stringify,
+        }}>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route
+              path="/"
+              element={<App />}
+            />
+          </Routes>
+        </QueryClientProvider>
+      </QueryParamProvider>
+    </BrowserRouter>
   </StrictMode>
 );

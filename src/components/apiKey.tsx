33f20127d5468776ad274c99d5b49f32b66bae41
BenTheMexican <bencarter@outlook.com>
2024-10-20 21:22:20 +0100

    add vercel.json

--- src/components/apiKey.tsx
+++ src/components/apiKey.tsx
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom';
 
 export const EnterApiKey = () => {
   const navigate = useNavigate();
-
   const [inputValue, setInputValue] = useState<string>('');
 
   return (
